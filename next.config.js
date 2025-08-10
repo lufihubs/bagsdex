49ef49d780d98b5ae3e6bf4e091c3861fbf20125
Nasabdul1 <abdulnasirudeen5@gmail.com>
2025-08-10 16:23:14 +0800

    Fix Netlify deployment configuration - added netlify.toml, updated next.config.js, installed Netlify Next.js plugin

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    unoptimized: true
+  },
+  env: {
+    CUSTOM_KEY: 'my-value',
+  }
+}
 
 module.exports = nextConfig;
